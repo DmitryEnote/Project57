5bcb9be07bdfa18277573cf1a981e97f8053b207
DmitryEnote <Enotegaming@gmail.com>
2023-09-01 15:15:51 +0300

    Finish
    End my work

---
+++ Project57/Assets/Scenes/ArraySumScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ArraySumScript : MonoBehaviour
+{
+    public Text result;
+
+
+    public void OnSumEvenNumbersInArray() //(string[] args)
+    
+    {
+        int count = 0 ;
+       int[] myArray = { 81, 22, 13, 54, 10, 34, 15, 26, 71, 68 };
+       for(int i=0; i<myArray.Length; i++ )
+       {
+        if (myArray[i] % 2 == 0)
+        {
+            count = count + myArray[i];
+            
+        }
+       }
+       result.text = count.ToString();
+        
+    }
+}   
+
